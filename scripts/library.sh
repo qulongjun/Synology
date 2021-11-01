9afd9b10faf118feb2694b33c368c3b677dc1c3f
jonnyqu <jonnyqu@tencent.com>
2021-11-01 17:36:12 +0800

    feat: remove file watcher

--- scripts/library.sh
+++ scripts/library.sh
@@ -2,6 +2,6 @@
 
 echo "触发 Library 脚本监听"
 
-# node ./watcher/libraryWatcher.js --all
+node ./watcher/libraryWatcher.js
 
-forever -o /volume1/日志中心/脚本日志/out/library.log -e /volume1/日志中心/脚本日志/err/library.log -w /volume1/script/Synology/watcher/libraryWatcher.js --all
+# forever -o /volume1/日志中心/脚本日志/out/library.log -e /volume1/日志中心/脚本日志/err/library.log -w /volume1/script/Synology/watcher/libraryWatcher.js --all
--- watcher/libraryWatcher.js
+++ watcher/libraryWatcher.js
@@ -1,24 +1,25 @@
 const path = require("path");
-const watcher = require("./watcher");
+// const watcher = require("./watcher");
 const config = require('../config');
 const { batch, init } = require("../utils/searchLibrary");
 
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
-const callbacks = function (watchType, file, status) {
-  if (watchType === "create") {
-    // 全部执行
-    if (args[0] === "--all") {
-      batch(config.LIBRARY_WATCH_PATH, config.LIBRARY_TARGET_PATH);
-    } else {
-      // 单个执行
-      init(file, config.LIBRARY_TARGET_PATH);
-    }
-  }
-};
+// const callbacks = function (watchType, file, status) {
+//   if (watchType === "create") {
+//     // 全部执行
+//     if (args[0] === "--all") {
+//       batch(config.LIBRARY_WATCH_PATH, config.LIBRARY_TARGET_PATH);
+//     } else {
+//       // 单个执行
+//       init(file, config.LIBRARY_TARGET_PATH);
+//     }
+//   }
+// };
 
 const initWatcher = function () {
-  watcher(config.LIBRARY_WATCH_PATH, callbacks);
+    batch(config.LIBRARY_WATCH_PATH, config.LIBRARY_TARGET_PATH);
+    //   watcher(config.LIBRARY_WATCH_PATH, callbacks);
 };
 
 initWatcher();
