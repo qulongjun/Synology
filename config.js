ac29b0eb437bdbeacb28dde5ba27658c46fd14c9
qulongjun <154256698@qq.com>
2021-11-01 17:34:05 +0800

    feat: update config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const path = require('path');
 const config = {
     IGNORE_FILES: ['.DS_Store'],
     LIBRARY_URL : 'https://www.seedmm.zone/',
-    LIBRARY_WATCH_PATH: path.resolve(__dirname, './demo'),
-    LIBRARY_TARGET_PATH: path.resolve(__dirname, './demo2')
+    LIBRARY_WATCH_PATH: '/volume3/cache/library',
+    LIBRARY_TARGET_PATH: '/volume2/video/成人影视'
 };
 
 
--- scripts/library.sh
+++ scripts/library.sh
@@ -4,4 +4,4 @@ echo "触发 Library 脚本监听"
 
 # node ./watcher/libraryWatcher.js --all
 
-forever -o library.log -e library.log -w ./watcher/libraryWatcher.js --all
+forever -o /volume1/日志中心/脚本日志/out/library.log -e /volume1/日志中心/脚本日志/err/library.log -w /volume1/script/Synology/watcher/libraryWatcher.js --all
